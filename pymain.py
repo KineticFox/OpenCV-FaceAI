d6c8b4493969184646a3590fde9b3fa44a29070c
KineticFox <felikiefer@web.de>
2019-05-10 12:19:03 +0200

    py tkinter

---
+++ pymain.py
@@ -0,0 +1,27 @@
+import tkinter
+import random
+
+
+class Window(tkinter.Frame):
+	def __init__(self, master=None):
+		super().__init__(master)
+		self.pack()
+		self.createWidgets()
+
+	def createWidgets(self):
+		self.nameEntry = tkinter.Entry(self)
+		self.nameEntry.pack()
+		self.textB = tkinter.StringVar()
+		self.textB.set(" ")
+		self.nameEntry["textvariable"] = self.textB
+		self.calc = tkinter.Button(self)
+		self.calc["text"] = "Calc"
+		self.calc["command"] = self.rand
+		self.calc.pack(side="left")
+
+	def rand(self):
+		self.textB.set(random.randint(1,21))
+
+root = tkinter.Tk()
+app = Window(root)
+app.mainloop()
